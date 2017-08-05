69bc78088c6d8856bc096d29b2bcaf4c6ae96aa2
richardse08 <eric.richards.finance@gmail.com>
2017-08-05 13:43:15 -0400

    Finally fixed AJAX request by using a Client ID publicly available from twitch.tv docs

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # twitch-viewer
 This app allows user to view information on different twitch.tv accounts
 
-There is an arror with the API call on this app, this has happened before and I will need to fix it again
-
-Originally, API call worked but then codepen's policy changed which resulted in needing to re-do API call
-
-I expect there is a similar issue now (210517)
+There have been a few changes to the twitch.tv API, and I have resolved to use a client ID. This is a public client ID and can be viewed in the twitch.tv documentation here: https://blog.twitch.tv/client-id-required-for-kraken-api-calls-afbb8e95f843
--- script.js
+++ script.js
@@ -1,48 +1,68 @@
    
 $(document).ready(function() { 
+    
+    
+    
+    
     var twitchArr = ["SayeedBlack", "ESL_SC2", "OgamingSC2", "lirik", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]
-
+    
+    
     for (i=0; i < twitchArr.length; i++) {
-
-        var urlBase = 'https://wind-bow.gomix.me/twitch-api'
-        var urlType = '/channels';
-        var urlFull = urlBase + urlType + '/' + twitchArr[i];
-        var notfound = 'https://cdn.instructables.com/FTU/1BBR/FLI8MT4O/FTU1BBRFLI8MT4O.MEDIUM.jpg';
-
-        $.getJSON(urlFull, function(data) {
-            var name = data.display_name;
-            var logo = data.logo;
-            var game = data.game;
-            var status = data.status;
-            var message = data.message;
-            var linker = data.url;
-            
-            var myhtml = "<div class='myresult'>";
-                myhtml += "<div class='row'>";
-                    myhtml += "<div class='col-md-1'>";    
-                    myhtml += "<a href=" + linker + ">";
-                    myhtml += "<img class='img-responsive' src ='" + logo + "'alt='no image found'>";
-                    myhtml += "</a>";
-                    myhtml += "</div>";
         
-                    myhtml += "<div class='col-md-2'>";   
-                    if (name == undefined) {
-                    } //display nothing
-                    else myhtml += "<div class ='name'>" + name + "</div>";
-                    myhtml += "</div>";
+        
+        
+        // Ajax request
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.twitch.tv/kraken/channels/' + twitchArr[i],
+            headers: {
+            'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+            },
+            success: function(data) {
                 
-                    if (message !== undefined) {
-                        myhtml += "<div class='message'>" + message + "</div>";
-                     }
-                    else if (game !== null) {
-                        myhtml += "<div class='col-md-2'>";
-                        myhtml += "<div class='game'>" + game + "</div>";
+                // Renaming some of the variables, this step can certainly be avoided
+                var name = data.display_name;
+                var logo = data.logo;
+                var game = data.game;
+                var status = data.status;
+                var message = data.message;
+                var linker = data.url;
+
+                var myhtml = "<div class='myresult'>";
+                    myhtml += "<div class='row'>";
+                        myhtml += "<div class='col-md-1'>";    
+                        myhtml += "<a href=" + linker + ">";
+                        myhtml += "<img class='img-responsive' src ='" + logo + "'alt='no image found'>";
+                        myhtml += "</a>";
                         myhtml += "</div>";
-                        myhtml += "<div class='status'>" + status + "</div>";
-                    } else myhtml += "<div class='game'>User Offline</div>";
-                myhtml += "</div>"; // row close
-                myhtml += "</div>"; // final div
-            $(".target").append(myhtml);
-        });
-    } //close loop                
+
+                        myhtml += "<div class='col-md-2'>";   
+                        if (name == undefined) {
+                        } //display nothing
+                        else myhtml += "<div class ='name'>" + name + "</div>";
+                        myhtml += "</div>";
+
+                        if (message !== undefined) {
+                            myhtml += "<div class='message'>" + message + "</div>";
+                         }
+                        else if (game !== null) {
+                            myhtml += "<div class='col-md-2'>";
+                            myhtml += "<div class='game'>" + game + "</div>";
+                            myhtml += "</div>";
+                            myhtml += "<div class='status'>" + status + "</div>";
+                        } else myhtml += "<div class='game'>User Offline</div>";
+                    myhtml += "</div>"; // row close
+                    myhtml += "</div>"; // final div
+                $(".target").append(myhtml);
+
+
+            } // End success
+
+
+        }); // End ajax
+            
+            
+    } //close loop        
+    
+    
 }); //document ready end
